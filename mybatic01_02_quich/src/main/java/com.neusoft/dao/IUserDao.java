de86d08c7544bfc68b526931251e4ac2a7953797
this-song1 <68591103+this-song1@users.noreply.github.com>
2020-09-03 17:07:24 +0800

    111

---
+++ mybatic01_02_quich/src/main/java/com.neusoft/dao/IUserDao.java
@@ -0,0 +1,12 @@
+package com.neusoft.dao;
+
+import com.neusoft.domain.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface IUserDao
+{
+    @Select("select * from user")
+    List<User> findAll();
+}
